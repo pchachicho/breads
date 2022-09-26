11fcf445e3071882239434ef34f2e8d99a1b9a78
Patrick hachicho <Hachichopatrick@gmail.com>
2022-09-26 18:36:09 -0400

    1

---
+++ controllers/breads_controller.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const breads = express.Router()
+
+// INDEX
+breads.get('/', (req, res)=> {
+    res.send('This is the index at /breads')
+})
+module.exports = breads
