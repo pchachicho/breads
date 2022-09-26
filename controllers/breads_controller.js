7c09baa6b7f80db93b4ad3a4afe2f5e495013b78
Patrick hachicho <Hachichopatrick@gmail.com>
2022-09-26 18:48:02 -0400

    sending index and show data

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -1,8 +1,13 @@
-const express = require('express')
-const breads = express.Router()
+const express = require("express");
+const breads = express.Router();
+const Bread = require("../models/bread.js");
 
 // INDEX
-breads.get('/', (req, res)=> {
-    res.send('This is the index at /breads')
-})
-module.exports = breads
+breads.get("/", (req, res) => {
+  res.send(Bread);
+});
+// SHOW
+breads.get("/:arrayIndex", (req, res) => {
+  res.send(Bread[req.params.arrayIndex]);
+});
+module.exports = breads;
